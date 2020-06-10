f0875cc84f125a4bb031e6a0e597b8a59c89c603
Fernando Mejia <fernandoemejia@yahoo.com>
2020-06-10 18:21:43 -0400

    Completed module2

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,15 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2*number;
+    }
+
+    private static int add(int [] numbers){
+        //return -1;
+        int sum=0;
+        for(int number: numbers){
+            sum=sum + number;
+        }
+        return sum;
     }
 }
